e235f5e8a81449592dc2d4461fd3805e79297656
konstantina paskova <konstantina.paskova@endava.com>
2019-01-20 21:25:37 +0200

    Removed main method from reverseList task

--- src/main/java/com/endava/arrays/ReverseList.java
+++ src/main/java/com/endava/arrays/ReverseList.java
@@ -7,13 +7,7 @@ public class ReverseList {
      * Write a function that reverses a list. For example: if inputArray = {1, 2, 3, 4},
      * the output of this function should be: {4, 3, 2, 1}.
      */
-    public static void main(String[] args) {
-        int[] array = new int[]{12, 4, 5, 2, 48};
-        //reverseAnArray(array);
-        reverseAnArrayInPlace(array);
-    }
-
-    public static int[] reverseAnArray(int[] inputArray) {
+    public  int[] reverseAnArray(int[] inputArray) {
         int[] newArray = new int[inputArray.length];
 
         for (int j = inputArray.length - 1; j >= 0; j--) {
@@ -26,8 +20,7 @@ public class ReverseList {
      * Modify the above method to reverse the input array in place. Instead of crating a new output array,
      * this method should switch the places of the elements and return nothing.
      */
-
-    public static void reverseAnArrayInPlace(int[] array) {
+    public void reverseAnArrayInPlace(int[] array) {
         for (int j = array.length - 1; j > array.length / 2; j--) {
             int tempValue = array[j];
             array[j] = array[array.length - j - 1];
