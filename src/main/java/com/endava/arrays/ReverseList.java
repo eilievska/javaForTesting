7fa35fbb7ec96426ae85dd6bff9e9541eff2a66b
konstantina paskova <konstantina.paskova@endava.com>
2019-01-20 21:13:29 +0200

    reverseList implementation

--- src/main/java/com/endava/arrays/ReverseList.java
+++ src/main/java/com/endava/arrays/ReverseList.java
@@ -7,16 +7,31 @@ public class ReverseList {
      * Write a function that reverses a list. For example: if inputArray = {1, 2, 3, 4},
      * the output of this function should be: {4, 3, 2, 1}.
      */
-    public int[] reverseAnArray(int[] inputArray) {
-        // Implement exercise.
-        return null;
+    public static void main(String[] args) {
+        int[] array = new int[]{12, 4, 5, 2, 48};
+        //reverseAnArray(array);
+        reverseAnArrayInPlace(array);
+    }
+
+    public static int[] reverseAnArray(int[] inputArray) {
+        int[] newArray = new int[inputArray.length];
+
+        for (int j = inputArray.length - 1; j >= 0; j--) {
+            newArray[inputArray.length - j - 1] = inputArray[j];
+        }
+        return newArray;
     }
 
     /**
      * Modify the above method to reverse the input array in place. Instead of crating a new output array,
      * this method should switch the places of the elements and return nothing.
      */
-    public void reverseAnArrayInPlace(int[] array) {
 
+    public static void reverseAnArrayInPlace(int[] array) {
+        for (int j = array.length - 1; j > array.length / 2; j--) {
+            int tempValue = array[j];
+            array[j] = array[array.length - j - 1];
+            array[array.length - j - 1] = tempValue;
+        }
     }
 }
